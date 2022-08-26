afa5e9db03b8537382a4d62e1d4adc48c45e667a
savoryboi <andbjerk@yahoo.com>
2022-08-26 10:17:25 -0500

    lowercase nav

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -6,15 +6,15 @@ function Navigation(props) {
         <div>
             <nav>
                 <Link className="link" to="/react-portfolio/about">
-                    <h2>About</h2>
+                    <h2>about</h2>
                 </Link>
                 <Link className="link" to="/react-portfolio/portfolio"><h2>portfolio</h2>
                 </Link>
                 <Link className="link" to="/react-portfolio/contact">
-                    <h2>Contact</h2>
+                    <h2>contact</h2>
                 </Link>
                 <Link className="link" to="/react-portfolio/resume">
-                    <h2>Resume</h2>
+                    <h2>resume</h2>
                 </Link>
 
             </nav>
