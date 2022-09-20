9d7215caf5b620faf809f02e80a25bec2976b8a5
savoryboi <andbjerk@yahoo.com>
2022-09-20 10:10:47 -0500

    background img and styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,27 @@
 
+
 function Contact() {
     return (
-        <div className="contact">
-            <span>Email me: andbjerk@yahoo.com</span>
-            <a href="https://www.linkedin.com/in/andy-bjerk/">Connect on LinkedIn</a>
+        <div className="contact-container">
+            <div>
+                <h2>get in touch with me</h2>
+            </div>
+
+            <div className="icon-wrapper">
+                <a target="_blank" href='http://linkedin.com/in/andy-bjerk' >
+                    <i className="fa-brands fa-linkedin fa-2x"></i>
+                </a>
+                <a target="_blank" href='http://github.com/savoryboi'>
+                    <i className="fa-brands fa-github fa-2x"></i>
+                </a>
+                <a target="_blank" href='http://twitter.com/savorycode'>
+                    <i class="fa-brands fa-twitter fa-2x"></i>
+                </a>
+                <a target="_blank" href='mailto: andbjerk@yahoo.com'>
+                    <i class="fa-regular fa-envelope fa-2x"></i>
+                </a>
+
+            </div>
         </div>
     )
 }
